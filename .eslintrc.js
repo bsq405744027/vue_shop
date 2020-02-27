1d573541ee404a25cab0c2c90b311d50de094f5e
巴圣淇 <bsq405744027@126.com>
2020-02-27 14:41:27 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  env: {
+    browser: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: 0,
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never' }
+    ],
+    'generator-star-spacing': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
